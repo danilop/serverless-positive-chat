39516f675957cbfe76dc12496a97b47dada3fa9d
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2019-02-21 15:52:53 +0100

    use $STACK_NAME in describe-stacks
    Replace hard-coded stack name with $STACK_NAME in describe-stacks call ;)

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ STACK_NAME=positive-chat
 sam build
 sam package --s3-bucket $BUCKET_PACKAGES --output-template-file packaged.yaml
 sam deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
-WSS_URI=$(aws cloudformation describe-stacks --stack-name positive-chat --query 'Stacks[0].Outputs[?OutputKey==`WebSocketURI`]'.OutputValue --output text)
+WSS_URI=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`WebSocketURI`]'.OutputValue --output text)
 aws s3 cp --recursive --acl public-read www/ s3://$BUCKET_WWW/
 echo $WSS_URI | aws s3 cp --acl public-read - s3://$BUCKET_WWW/wss-uri.txt
